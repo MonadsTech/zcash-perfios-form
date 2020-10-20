279950723052c8e68588f6e83f9b20b66319c116
Pankaj Vaghela <vpankaj1998official@gmail.com>
2020-10-20 20:17:48 +0530

    example integration

--- src/modules/enach/consumerData.js
+++ src/modules/enach/consumerData.js
@@ -4,14 +4,17 @@ export const ConsumerDataDefault = {
   returnUrl: "https://postman-echo.com/post",
   // 'responseHandler': handleResponse, //custom fn @pankajvaghela
 
-  paymentMode: "netBanking", // cant be alll cuz we are passing it ourselves
+  // paymentMode: "all", // cant be alll cuz we are passing it ourselves
+  paymentMode: "netBanking", // cant be all cuz we are  passing account data ourselves
   merchantLogoUrl:
     "https://res.cloudinary.com/dhoiqmk4x/image/upload/v1595829219/ZCash_Logo_1_n2cdan.png", //provided merchant logo will be displayed
   merchantId: "T596349", // merchant code
   currency: "INR",
   // 'consumerId': 'c964634',// only form SI   //Your unique consumer identifier to register a eMandate/eNACH
   consumerMobileNo: "9999999911", //* from opty
-  consumerEmailId: "test@test.com", //* from opty
+  // consumerMobileNo: "", //* from opty
+  consumerEmailId: "test@gmail.com", //* from opty
+  // consumerEmailId: "", //* from opty
   txnId: "1481197581115", //* same as token api   //Unique merchant transaction ID
   txnType: "SALE",
   txnSubType: "DEBIT",
@@ -29,16 +32,18 @@ export const ConsumerDataDefault = {
     BUTTON_COLOR_CODE_2: "#FFFFFF", //provide merchant's suitable color code for button text
   },
   //'accountNo': '1234567890',//* form    //Pass this if accountNo is captured at merchant side for eMandate/eNACH
+  // accountNo: "", //* form    //Pass this if accountNo is captured at merchant side for eMandate/eNACH
   //'accountHolderName': 'Name',//* form  //Pass this if accountHolderName is captured at merchant side for ICICI eMandate & eNACH registration this is mandatory field, if not passed from merchant Customer need to enter in Checkout UI.
   //'ifscCode': 'ICIC0000001',//* form    //Pass this if ifscCode is captured at merchant side.
-  accountType: "Saving", //! form?  //Required for eNACH registration this is mandatory field
+  accountType: "Saving", //  //Required for eNACH registration this is mandatory field
 
-  //* enableSI pe chahiye
-  // 'debitStartDate': '10-03-2019',//! form?
-  // 'debitEndDate': '01-03-2047',//! form?
-  // 'maxAmount': '100',//! form?
-  // 'amountType': 'M',//! form?
-  // 'frequency': 'ADHO',//! form?	//  Available options DAIL, WEEK, MNTH, QURT, MIAN, YEAR, BIMN and ADHO
+  //* only for when enableSI is true
+  // 'debitStartDate': '10-03-2019',//! for enableSI
+  // 'debitEndDate': '01-03-2047',//! for enableSI
+  // 'maxAmount': '100',//! for enableSI
+  // 'amountType': 'M',//! for enableSI
+  // 'frequency': 'ADHO',//! for enableSI	//  Available options DAIL, WEEK, MNTH, QURT, MIAN, YEAR, BIMN and ADHO
 
-  bankCode: 9480,
+  // bankCode: 9480,// AXIS
+  bankCode: 110050, // SBI
 };
--- src/screens/EnachScreen.js
+++ src/screens/EnachScreen.js
@@ -89,30 +89,43 @@ const EnachScreen = () => {
           hideSIConfirmation: false,
           showSIResponseMsg: false,
           enableNewWindowFlow: false,
+          // ...ExampleData.features, // uncomment this to use example data
         },
         consumerData: {
           ...consumerData,
+          // ...ExampleData.consumerData, // uncomment this to use example data
           token: apiToken,
           enachResponseHandler,
         },
       };
       console.log("EnachScreen -> configJson", configJson);
 
+      // window.$.pnCheckout(ExampleData); // for example data
       window.$.pnCheckout(configJson);
     }
   }, [apiToken, consumerData, enachResponseHandler, enachTokenApiStatus]);
 
   React.useEffect(() => {
+    const _data = consumerData;
+    // const _data = ExampleData.consumerData; //  uncomment this to use example data
+
+    // if (consumerData?.deviceId) { //  uncomment this to while using example data
     if (consumerData?.accountNo) {
       setEnachTokenApiStatus(API_STATUS.LOADING);
 
+      const totalAmount = _data.items.reduce(
+        (sum, item) => (sum += Number.parseInt(item.amount)),
+        0
+      );
+      console.log("EnachScreen -> totalAmount", totalAmount);
       const dataForTokenAPI = {
-        txn_id: consumerData.txnId,
-        total_amount: "1", // make it summation later
-        account_number: consumerData.accountNo,
+        txn_id: _data.txnId,
+        total_amount: `${totalAmount}`, // make it summation later
+        account_number: _data.accountNo,
         consumer_id: "",
-        email_id: consumerData.consumerEmailId,
-        mobile_number: consumerData.consumerMobileNo,
+        // consumer_id: _data.consumerId, //for example data
+        email_id: _data.consumerEmailId,
+        mobile_number: _data.consumerMobileNo,
         debit_start_date: "",
         debit_end_date: "",
         max_amount: "",
@@ -122,6 +135,7 @@ const EnachScreen = () => {
         exp_month: "",
         exp_year: "",
         cvv_code: "",
+        merchantId: _data.merchantId,
       };
 
       console.log("EnachScreen -> dataForTokenAPI", dataForTokenAPI);
@@ -129,6 +143,7 @@ const EnachScreen = () => {
       fetch(ENACH_TOKEN_API, {
         method: "POST",
         headers: {
+          Accept: "application/json",
           "Content-Type": "application/json",
         },
         body: JSON.stringify(dataForTokenAPI),
@@ -185,3 +200,58 @@ const EnachScreen = () => {
 };
 
 export { EnachScreen };
+
+const handleEgResponse = (res) => {
+  console.log("handleEgResponse -> res)", res);
+};
+const ExampleData = {
+  tarCall: false,
+  features: {
+    showPGResponseMsg: true,
+    enableAbortResponse: true,
+    enableNewWindowFlow: false, //for hybrid applications please disable this by passing false
+    enableExpressPay: true,
+    siDetailsAtMerchantEnd: true,
+    enableSI: true,
+  },
+  consumerData: {
+    deviceId: "WEBMD5", //possible values 'WEBSH1', 'WEBSH2' and 'WEBMD5'
+    token:
+      "37b0b9dd68cceafdf8dd38142803c0babc7073c3042547dc6060660ba50ac5cd043ddf0a27bdcbddc2cab2a0a28cbf51b30ecc2176e617304745f825655b1ff9",
+    returnUrl:
+      "https://www.tekprocess.co.in/MerchantIntegrationClient/MerchantResponsePage.jsp", //merchant response page URL
+    responseHandler: handleEgResponse,
+    paymentMode: "netBanking",
+    merchantLogoUrl: "https://www.paynimo.com/CompanyDocs/company-logo-md.png", //provided merchant logo will be displayed
+    merchantId: "L3348",
+    currency: "INR",
+    // consumerId: "c964634", //Your unique consumer identifier to register a eMandate/eNACH
+    consumerId: "T596349",
+    consumerMobileNo: "9876543210",
+    consumerEmailId: "test@test.com",
+    txnId: "1481197581115", //Unique merchant transaction ID
+    items: [
+      {
+        itemId: "test",
+        amount: "1",
+        comAmt: "0",
+      },
+    ],
+    customStyle: {
+      PRIMARY_COLOR_CODE: "#3977b7", //merchant primary color code
+      SECONDARY_COLOR_CODE: "#FFFFFF", //provide merchant's suitable color code
+      BUTTON_COLOR_CODE_1: "#1969bb", //merchant's button background color code
+      BUTTON_COLOR_CODE_2: "#FFFFFF", //provide merchant's suitable color code for button text
+    },
+    accountNo: "", //Pass this if accountNo is captured at merchant side for eMandate/eNACH
+    //'accountNo': '1234567890',    //Pass this if accountNo is captured at merchant side for eMandate/eNACH
+    //'accountHolderName': 'Name',  //Pass this if accountHolderName is captured at merchant side for ICICI eMandate & eNACH registration this is mandatory field, if not passed from merchant Customer need to enter in Checkout UI.
+    //'ifscCode': 'ICIC0000001',        //Pass this if ifscCode is captured at merchant side.
+    // accountType: "Saving", //Required for eNACH registration this is mandatory field
+    // debitStartDate: "10-03-2019",
+    // debitEndDate: "01-03-2047",
+    // maxAmount: "100",
+    // amountType: "M",
+    // frequency: "ADHO", //  Available options DAIL, WEEK, MNTH, QURT, MIAN, YEAR, BIMN and ADHO
+  },
+};
