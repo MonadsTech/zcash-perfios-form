36bf85ab753791ea1b9c3617acd005160670a190
Pankaj Vaghela <vpankaj1998official@gmail.com>
2020-10-21 17:20:45 +0530

    enach screen lint error

--- src/screens/EnachScreen.js
+++ src/screens/EnachScreen.js
@@ -201,57 +201,57 @@ const EnachScreen = () => {
 
 export { EnachScreen };
 
-const handleEgResponse = (res) => {
-  console.log("handleEgResponse -> res)", res);
-};
-const ExampleData = {
-  tarCall: false,
-  features: {
-    showPGResponseMsg: true,
-    enableAbortResponse: true,
-    enableNewWindowFlow: false, //for hybrid applications please disable this by passing false
-    enableExpressPay: true,
-    siDetailsAtMerchantEnd: true,
-    enableSI: true,
-  },
-  consumerData: {
-    deviceId: "WEBMD5", //possible values 'WEBSH1', 'WEBSH2' and 'WEBMD5'
-    token:
-      "37b0b9dd68cceafdf8dd38142803c0babc7073c3042547dc6060660ba50ac5cd043ddf0a27bdcbddc2cab2a0a28cbf51b30ecc2176e617304745f825655b1ff9",
-    returnUrl:
-      "https://www.tekprocess.co.in/MerchantIntegrationClient/MerchantResponsePage.jsp", //merchant response page URL
-    responseHandler: handleEgResponse,
-    paymentMode: "netBanking",
-    merchantLogoUrl: "https://www.paynimo.com/CompanyDocs/company-logo-md.png", //provided merchant logo will be displayed
-    merchantId: "L3348",
-    currency: "INR",
-    // consumerId: "c964634", //Your unique consumer identifier to register a eMandate/eNACH
-    consumerId: "T596349",
-    consumerMobileNo: "9876543210",
-    consumerEmailId: "test@test.com",
-    txnId: "1481197581115", //Unique merchant transaction ID
-    items: [
-      {
-        itemId: "test",
-        amount: "1",
-        comAmt: "0",
-      },
-    ],
-    customStyle: {
-      PRIMARY_COLOR_CODE: "#3977b7", //merchant primary color code
-      SECONDARY_COLOR_CODE: "#FFFFFF", //provide merchant's suitable color code
-      BUTTON_COLOR_CODE_1: "#1969bb", //merchant's button background color code
-      BUTTON_COLOR_CODE_2: "#FFFFFF", //provide merchant's suitable color code for button text
-    },
-    accountNo: "", //Pass this if accountNo is captured at merchant side for eMandate/eNACH
-    //'accountNo': '1234567890',    //Pass this if accountNo is captured at merchant side for eMandate/eNACH
-    //'accountHolderName': 'Name',  //Pass this if accountHolderName is captured at merchant side for ICICI eMandate & eNACH registration this is mandatory field, if not passed from merchant Customer need to enter in Checkout UI.
-    //'ifscCode': 'ICIC0000001',        //Pass this if ifscCode is captured at merchant side.
-    // accountType: "Saving", //Required for eNACH registration this is mandatory field
-    // debitStartDate: "10-03-2019",
-    // debitEndDate: "01-03-2047",
-    // maxAmount: "100",
-    // amountType: "M",
-    // frequency: "ADHO", //  Available options DAIL, WEEK, MNTH, QURT, MIAN, YEAR, BIMN and ADHO
-  },
-};
+// const handleEgResponse = (res) => {
+//   console.log("handleEgResponse -> res)", res);
+// };
+// const ExampleData = {
+//   tarCall: false,
+//   features: {
+//     showPGResponseMsg: true,
+//     enableAbortResponse: true,
+//     enableNewWindowFlow: false, //for hybrid applications please disable this by passing false
+//     enableExpressPay: true,
+//     siDetailsAtMerchantEnd: true,
+//     enableSI: true,
+//   },
+//   consumerData: {
+//     deviceId: "WEBMD5", //possible values 'WEBSH1', 'WEBSH2' and 'WEBMD5'
+//     token:
+//       "37b0b9dd68cceafdf8dd38142803c0babc7073c3042547dc6060660ba50ac5cd043ddf0a27bdcbddc2cab2a0a28cbf51b30ecc2176e617304745f825655b1ff9",
+//     returnUrl:
+//       "https://www.tekprocess.co.in/MerchantIntegrationClient/MerchantResponsePage.jsp", //merchant response page URL
+//     responseHandler: handleEgResponse,
+//     paymentMode: "netBanking",
+//     merchantLogoUrl: "https://www.paynimo.com/CompanyDocs/company-logo-md.png", //provided merchant logo will be displayed
+//     merchantId: "L3348",
+//     currency: "INR",
+//     // consumerId: "c964634", //Your unique consumer identifier to register a eMandate/eNACH
+//     consumerId: "T596349",
+//     consumerMobileNo: "9876543210",
+//     consumerEmailId: "test@test.com",
+//     txnId: "1481197581115", //Unique merchant transaction ID
+//     items: [
+//       {
+//         itemId: "test",
+//         amount: "1",
+//         comAmt: "0",
+//       },
+//     ],
+//     customStyle: {
+//       PRIMARY_COLOR_CODE: "#3977b7", //merchant primary color code
+//       SECONDARY_COLOR_CODE: "#FFFFFF", //provide merchant's suitable color code
+//       BUTTON_COLOR_CODE_1: "#1969bb", //merchant's button background color code
+//       BUTTON_COLOR_CODE_2: "#FFFFFF", //provide merchant's suitable color code for button text
+//     },
+//     accountNo: "", //Pass this if accountNo is captured at merchant side for eMandate/eNACH
+//     //'accountNo': '1234567890',    //Pass this if accountNo is captured at merchant side for eMandate/eNACH
+//     //'accountHolderName': 'Name',  //Pass this if accountHolderName is captured at merchant side for ICICI eMandate & eNACH registration this is mandatory field, if not passed from merchant Customer need to enter in Checkout UI.
+//     //'ifscCode': 'ICIC0000001',        //Pass this if ifscCode is captured at merchant side.
+//     // accountType: "Saving", //Required for eNACH registration this is mandatory field
+//     // debitStartDate: "10-03-2019",
+//     // debitEndDate: "01-03-2047",
+//     // maxAmount: "100",
+//     // amountType: "M",
+//     // frequency: "ADHO", //  Available options DAIL, WEEK, MNTH, QURT, MIAN, YEAR, BIMN and ADHO
+//   },
+// };
