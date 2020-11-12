df735732588e580a98eb27751bc4c4f86b1f2128
RAJAT VIJAY <rajatvijay5@gmail.com>
2020-11-12 14:25:16 +0530

    ENACH fixes

--- src/modules/enach/EnachUserForm.js
+++ src/modules/enach/EnachUserForm.js
@@ -1,65 +1,56 @@
 import React from "react";
 import { Button, Form, Input } from "antd";
 
-const layout = {
-  labelCol: {
-    span: 4,
-    md: {
-      offset: 2,
-    },
-  },
-  wrapperCol: {
-    span: 12,
-  },
-};
-
-export const EnachUserForm = (props) => {
-  const { onFinish } = props;
+const EnachUserForm = (props) => {
+  const { onFinish, loading, initialValues } = props;
 
   return (
-    <Form {...layout} name="nest-messages" onFinish={onFinish}>
+    <Form
+      layout="vertical"
+      name="nest-messages"
+      onFinish={onFinish}
+      initialValues={initialValues}
+    >
+      {/* consumerMobileNo */}
       <Form.Item
-        name={"accountNo"}
-        label={"accountNo"}
-        rules={[
-          {
-            required: true,
-          },
-        ]}
-        initialValue=""
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        name={"accountHolderName"}
-        label={"accountHolderName"}
-        rules={[
-          {
-            required: true,
-          },
-        ]}
-        initialValue=""
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        name={"ifscCode"}
-        label={"ifscCode"}
-        rules={[
-          {
-            required: true,
-          },
-        ]}
-        initialValue=""
+        required="true"
+        name="consumerMobileNo"
+        label="consumerMobileNo"
       >
         <Input />
       </Form.Item>
 
-      <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 10 }}>
-        <Button type="primary" htmlType="submit">
+      {/* consumerEmailId */}
+      <Form.Item required="true" name="consumerEmailId" label="consumerEmailId">
+        <Input />
+      </Form.Item>
+
+      {/* accountNo */}
+      <Form.Item required="true" name="accountNo" label="accountNo">
+        <Input />
+      </Form.Item>
+
+      {/* accountHolderName */}
+      <Form.Item
+        name="accountHolderName"
+        label="accountHolderName"
+        required="true"
+      >
+        <Input />
+      </Form.Item>
+
+      {/* ifscCode */}
+      <Form.Item name="ifscCode" label="ifscCode" required="true">
+        <Input />
+      </Form.Item>
+
+      <Form.Item style={{ textAlign: "center" }}>
+        <Button loading={loading} type="primary" htmlType="submit">
           Submit
         </Button>
       </Form.Item>
     </Form>
   );
 };
+
+export { EnachUserForm };
--- src/screens/EnachScreen.js
+++ src/screens/EnachScreen.js
@@ -1,199 +1,235 @@
-import { Layout } from "antd";
-import React from "react";
-import { LoadingIndicator } from "../common/components/LoadingIndicator";
+import { Layout, message } from "antd";
+import React, { useEffect, useState } from "react";
 import { API_STATUS } from "../common/utils/api";
 import { getAllQueryParams } from "../common/utils/url";
-import { ConsumerDataDefault } from "../modules/enach/consumerData";
 import { EnachUserForm } from "../modules/enach/EnachUserForm";
-
-// import { UserDataForm } from "./modules/UserDataForm";
-// http://localhost:3000/enach?email=ankurj630@gmail.com&loan_amount=1000&loan_duration=24&loan_type=Home&callback_url=https://google.com&txn_id=PQ1342687YTX#edfg
+import { Helmet } from "react-helmet";
+import { to } from "await-to-js";
+import { v4 as uuidV4 } from "uuid";
 
 const ENACH_TOKEN_API = `https://zavron.byts.in/v1/payment/enach/token`;
+const MERCHANT_ID = "T596349";
+const INITIAL_VALUES = {
+  consumerMobileNo: "7877523772",
+  consumerEmailId: "rajatvijay5@gmail.com",
+  accountNo: "919010053812087",
+  accountHolderName: "Rajat Vijay",
+  ifscCode: "UTIB0000335",
+};
 
-const JQUERY_URL =
-  "https://www.paynimo.com/Paynimocheckout/client/lib/jquery.min.js";
-const PAYNIMO_URL =
-  "https://www.paynimo.com/Paynimocheckout/server/lib/checkout.js";
-
-const buildScriptTag = (src, onload) => {
-  const script = document.createElement("script");
-  script.src = src;
-  script.async = true;
-  script.onload = onload;
-  return script;
+/**
+ * Calls the API to fetch token,
+ * using data from app, default config and form data
+ * Returns promises that resolves to the token
+ * @param {Record<string,string>} payload
+ */
+const getToken = (payload) => {
+  return fetch(ENACH_TOKEN_API, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((response) => response.payload.token);
 };
 
 const EnachScreen = () => {
-  const [consumerData, setConsumerData] = React.useState(null);
+  const [initialFormData, setInitialFormData] = useState(INITIAL_VALUES);
+  const [status, setStatus] = useState(API_STATUS.IDLE);
 
-  const [apiToken, setApiToken] = React.useState(null);
-  const [enachTokenApiStatus, setEnachTokenApiStatus] = React.useState(
-    API_STATUS.IDLE
-  );
-
-  React.useEffect(() => {
+  /**
+   * 1. Load data from the app aka consumerData
+   * 2. Load data from the query params aka queryData
+   * 3. Merge them in the same order
+   * consumerData, queryData =
+   * {
+   *  consumerMobileNo: string,
+   *  consumerEmailId: string,
+   *  accountNo: string,
+   *  accountHolderName: string,
+   *  ifscCode: string
+   * }
+   */
+  useEffect(() => {
     const queryData = getAllQueryParams();
     console.log("EnachScreen -> queryData", queryData);
 
-    let AppConsumerData = window.Enach?.ConsumerData || {};
-
-    if (queryData.dummy === "1") {
-      delete queryData.dummy;
-      setConsumerData({
-        ...ConsumerDataDefault,
-        ...AppConsumerData,
-        ...queryData,
-      });
-    } else if (AppConsumerData) {
-      setConsumerData(AppConsumerData);
-    }
-  }, []);
-
-  const loadEnachScript = React.useCallback(() => {
-    const script = buildScriptTag(PAYNIMO_URL, () => {
-      console.log("Enach script loaded");
+    const dataFromApp = window.Enach?.consumerData || {};
+    setInitialFormData({
+      ...dataFromApp,
+      ...queryData,
     });
-    document.body.appendChild(script);
   }, []);
 
-  React.useEffect(() => {
-    const script = buildScriptTag(JQUERY_URL, () => {
-      console.log("JQUERY script loaded");
-      loadEnachScript();
+  /**
+   * Called when the user submits the form
+   * Orchestrate the main data flow
+   * data from -> calling ENACH SDK
+   * @param {{
+   *  consumerMobileNo: string,
+   *  consumerEmailId: string,
+   *  accountNo: string,
+   *  accountHolderName: string,
+   *  ifscCode: string
+   * }} formValues
+   * @returns {void}
+   */
+  const handleFinish = async (formValues) => {
+    console.log("handleFinish -> formValues", formValues);
+    // const txnId = "3ba0bd3a5c7d42908ee25dcb2f57e29b";
+    const txnId = uuidV4().split("-").join("");
+    const {
+      accountNo,
+      consumerMobileNo,
+      consumerEmailId,
+      accountHolderName,
+      ifscCode,
+    } = formValues;
+    const token = await getTokenUsingFormData({
+      txnId,
+      accountNo,
+      consumerMobileNo,
+      consumerEmailId,
     });
-    document.body.appendChild(script);
-  }, [loadEnachScript]);
-
-  const enachResponseHandler = React.useCallback((res) => {
-    console.log("enachResponseHandler -> res", res);
-  }, []);
-
-  React.useEffect(() => {
-    if (apiToken && enachTokenApiStatus === API_STATUS.RESOLVED) {
-      console.log("EnachScreen -> apiToken", apiToken);
-
-      var configJson = {
-        tarCall: false,
-        features: {
-          showPGResponseMsg: true,
-          enableAbortResponse: true,
-          enableExpressPay: false,
-          payDetailsAtMerchantEnd: true,
-          enableSI: false,
-          showAllModesWithSI: false,
-          siDetailsAtMerchantEnd: false,
-          hideSIDetails: false,
-          enableDebitDay: false,
-          expandSIDetails: false,
-          hideSIConfirmation: false,
-          showSIResponseMsg: false,
-          enableNewWindowFlow: false,
-          // ...ExampleData.features, // uncomment this to use example data
-        },
-        consumerData: {
-          ...consumerData,
-          // ...ExampleData.consumerData, // uncomment this to use example data
-          token: apiToken,
-          enachResponseHandler,
-        },
-      };
-      console.log("EnachScreen -> configJson", configJson);
-
-      // window.$.pnCheckout(ExampleData); // for example data
-      window.$.pnCheckout(configJson);
+    const finalConfig = getENACHConfig({
+      token,
+      responseHandler,
+      consumerMobileNo,
+      consumerEmailId,
+      bankCode: 9480,
+      accountNo,
+      accountHolderName,
+      ifscCode,
+      txnId,
+    });
+    if (token) {
+      openENACHModal(finalConfig);
     }
-  }, [apiToken, consumerData, enachResponseHandler, enachTokenApiStatus]);
-
-  React.useEffect(() => {
-    const _data = consumerData;
-    // const _data = ExampleData.consumerData; //  uncomment this to use example data
-
-    // if (consumerData?.deviceId) { //  uncomment this to while using example data
-    if (consumerData?.accountNo) {
-      setEnachTokenApiStatus(API_STATUS.LOADING);
-
-      const totalAmount = _data.items.reduce(
-        (sum, item) => (sum += Number.parseInt(item.amount)),
-        0
-      );
-      console.log("EnachScreen -> totalAmount", totalAmount);
-      const dataForTokenAPI = {
-        txn_id: _data.txnId,
-        total_amount: `${totalAmount}`, // make it summation later
-        account_number: _data.accountNo,
-        consumer_id: "",
-        // consumer_id: _data.consumerId, //for example data
-        email_id: _data.consumerEmailId,
-        mobile_number: _data.consumerMobileNo,
-        debit_start_date: "",
-        debit_end_date: "",
-        max_amount: "",
-        amount_type: "",
-        frequency: "",
-        card_number: "",
-        exp_month: "",
-        exp_year: "",
-        cvv_code: "",
-        merchantId: _data.merchantId,
-      };
-
-      console.log("EnachScreen -> dataForTokenAPI", dataForTokenAPI);
-
-      fetch(ENACH_TOKEN_API, {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(dataForTokenAPI),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.status) {
-            setApiToken(data.payload.token);
-            setEnachTokenApiStatus(API_STATUS.RESOLVED);
-          } else {
-            console.log(data.message);
-            alert(JSON.stringify(data.message));
-            setEnachTokenApiStatus(API_STATUS.REJECTED);
-          }
-        })
-        .catch((err) => {
-          setApiToken(null);
-          setEnachTokenApiStatus(API_STATUS.REJECTED);
-        });
-    }
-  }, [setEnachTokenApiStatus, consumerData]);
-
-  const onUserFormFinish = (formValues) => {
-    console.log("onUserFormFinish -> consumerData", consumerData);
-
-    setConsumerData((value) => ({
-      ...value,
-      ...formValues,
-    }));
   };
 
-  const hasToken = apiToken && !!Object.keys(apiToken).length;
+  /**
+   * Fetches token from Zavron backend
+   * @param {{
+   *  txnId: string,
+   *  accountNo: string,
+   *  consumerEmailId: string,
+   *  consumerMobileNo: string,
+   * }} config
+   */
+  const getTokenUsingFormData = async ({
+    txnId,
+    accountNo,
+    consumerEmailId,
+    consumerMobileNo,
+  }) => {
+    setStatus(API_STATUS.LOADING);
+
+    const payload = {
+      txn_id: txnId,
+      total_amount: "1",
+      account_number: accountNo,
+      email_id: consumerEmailId,
+      mobile_number: consumerMobileNo,
+      debit_start_date: "",
+      consumer_id: "",
+      debit_end_date: "",
+      max_amount: "",
+      amount_type: "",
+      frequency: "",
+      card_number: "",
+      exp_month: "",
+      exp_year: "",
+      cvv_code: "",
+      merchantId: MERCHANT_ID,
+    };
+
+    console.log("EnachScreen: getTokenUsingFormData payload ", payload);
+
+    // API call
+    setStatus(API_STATUS.LOADING);
+    const [error, token] = await to(getToken(payload));
+    if (!error && token) {
+      setStatus(API_STATUS.RESOLVED);
+      return token;
+    } else {
+      setStatus(API_STATUS.REJECTED);
+      message.error(error.message);
+      return null;
+    }
+  };
+
+  /**
+   * Opens the ENACH Modal using the config
+   * @param {Record<string,string>} config Toke for ENACH
+   */
+  const openENACHModal = (config) => {
+    console.log("EnachScreen: openENACHModal config ", config);
+
+    const configJson = {
+      tarCall: false,
+      features: {
+        showPGResponseMsg: true,
+        enableAbortResponse: true,
+        enableExpressPay: false,
+        payDetailsAtMerchantEnd: true,
+        enableSI: false,
+        showAllModesWithSI: false,
+        siDetailsAtMerchantEnd: false,
+        hideSIDetails: false,
+        enableDebitDay: false,
+        expandSIDetails: false,
+        hideSIConfirmation: false,
+        showSIResponseMsg: false,
+        enableNewWindowFlow: false,
+      },
+      consumerData: config,
+    };
+    console.log("EnachScreen: openENACHModal payload ", configJson);
+
+    window.$.pnCheckout(configJson);
+  };
+
+  /**
+   * TODO: Implement me!
+   * Callback from ENACH SDK
+   * @param {string} res
+   */
+  const responseHandler = (res) => {
+    console.log("enachResponseHandler -> res", res);
+  };
 
   return (
     <Layout
       style={{
         padding: "50px 20px",
-        // alignItems: "center",
-        // textAlign: "center",
       }}
     >
-      <EnachUserForm onFinish={onUserFormFinish} />
-      {/* {hasUserData && <UserDataForm userData={consumerData} />} */}
-      {enachTokenApiStatus === API_STATUS.LOADING && <LoadingIndicator />}
-      {/* {enachTokenApiStatus === API_STATUS.RESOLVED && hasToken && ( */}
-      {/* // <PerfiosForm payloadData={apiToken} /> */}
-      {/* )} */}
-      {(enachTokenApiStatus === API_STATUS.REJECTED ||
-        (enachTokenApiStatus === API_STATUS.RESOLVED && !hasToken)) && (
-        <p>Some error occurred, please contact support</p>
+      <Helmet>
+        <meta
+          name="viewport"
+          content="user-scalable=no, width=device-width, initial-scale=1"
+        />
+        <script
+          src="https://www.tecprocesssolution.com/proto/p2m/client/lib/jquery.min.js"
+          type="text/javascript"
+        ></script>
+        <script
+          type="text/javascript"
+          src="https://www.paynimo.com/paynimocheckout/server/lib/checkout.js"
+        ></script>
+      </Helmet>
+      <EnachUserForm
+        initialValues={initialFormData}
+        loading={status === API_STATUS.LOADING}
+        onFinish={handleFinish}
+      />
+      {status === API_STATUS.REJECTED && (
+        <p style={{ color: "red" }}>
+          Some error occurred, please contact support
+        </p>
       )}
     </Layout>
   );
@@ -201,57 +237,55 @@ const EnachScreen = () => {
 
 export { EnachScreen };
 
-// const handleEgResponse = (res) => {
-//   console.log("handleEgResponse -> res)", res);
-// };
-// const ExampleData = {
-//   tarCall: false,
-//   features: {
-//     showPGResponseMsg: true,
-//     enableAbortResponse: true,
-//     enableNewWindowFlow: false, //for hybrid applications please disable this by passing false
-//     enableExpressPay: true,
-//     siDetailsAtMerchantEnd: true,
-//     enableSI: true,
-//   },
-//   consumerData: {
-//     deviceId: "WEBMD5", //possible values 'WEBSH1', 'WEBSH2' and 'WEBMD5'
-//     token:
-//       "37b0b9dd68cceafdf8dd38142803c0babc7073c3042547dc6060660ba50ac5cd043ddf0a27bdcbddc2cab2a0a28cbf51b30ecc2176e617304745f825655b1ff9",
-//     returnUrl:
-//       "https://www.tekprocess.co.in/MerchantIntegrationClient/MerchantResponsePage.jsp", //merchant response page URL
-//     responseHandler: handleEgResponse,
-//     paymentMode: "netBanking",
-//     merchantLogoUrl: "https://www.paynimo.com/CompanyDocs/company-logo-md.png", //provided merchant logo will be displayed
-//     merchantId: "L3348",
-//     currency: "INR",
-//     // consumerId: "c964634", //Your unique consumer identifier to register a eMandate/eNACH
-//     consumerId: "T596349",
-//     consumerMobileNo: "9876543210",
-//     consumerEmailId: "test@test.com",
-//     txnId: "1481197581115", //Unique merchant transaction ID
-//     items: [
-//       {
-//         itemId: "test",
-//         amount: "1",
-//         comAmt: "0",
-//       },
-//     ],
-//     customStyle: {
-//       PRIMARY_COLOR_CODE: "#3977b7", //merchant primary color code
-//       SECONDARY_COLOR_CODE: "#FFFFFF", //provide merchant's suitable color code
-//       BUTTON_COLOR_CODE_1: "#1969bb", //merchant's button background color code
-//       BUTTON_COLOR_CODE_2: "#FFFFFF", //provide merchant's suitable color code for button text
-//     },
-//     accountNo: "", //Pass this if accountNo is captured at merchant side for eMandate/eNACH
-//     //'accountNo': '1234567890',    //Pass this if accountNo is captured at merchant side for eMandate/eNACH
-//     //'accountHolderName': 'Name',  //Pass this if accountHolderName is captured at merchant side for ICICI eMandate & eNACH registration this is mandatory field, if not passed from merchant Customer need to enter in Checkout UI.
-//     //'ifscCode': 'ICIC0000001',        //Pass this if ifscCode is captured at merchant side.
-//     // accountType: "Saving", //Required for eNACH registration this is mandatory field
-//     // debitStartDate: "10-03-2019",
-//     // debitEndDate: "01-03-2047",
-//     // maxAmount: "100",
-//     // amountType: "M",
-//     // frequency: "ADHO", //  Available options DAIL, WEEK, MNTH, QURT, MIAN, YEAR, BIMN and ADHO
-//   },
-// };
+function getENACHConfig({
+  token,
+  responseHandler,
+  consumerMobileNo,
+  consumerEmailId,
+  bankCode,
+  accountNo,
+  accountHolderName,
+  ifscCode,
+  txnId,
+}) {
+  return {
+    deviceId: "WEBSH2",
+    token,
+    enableNewWindowFlow: false,
+    returnUrl: "https://postman-echo.com/post",
+    responseHandler,
+    paymentMode: "netBanking",
+    customStyle: {
+      PRIMARY_COLOR_CODE: "#FFFFFF",
+      SECONDARY_COLOR_CODE: "#001F79",
+      BUTTON_COLOR_CODE_1: "#000F3D",
+      BUTTON_COLOR_CODE_2: "#FFFFFF",
+    },
+    merchantLogoUrl:
+      "https://res.cloudinary.com/dhoiqmk4x/image/upload/v1595829219/ZCash_Logo_1_n2cdan.png", //provided merchant logo will be displayed
+    merchantMsg: "",
+    disclaimerMsg: "",
+    merchantId: MERCHANT_ID,
+    consumerMobileNo,
+    consumerEmailId,
+    txnId,
+    txnType: "SALE",
+    txnSubType: "DEBIT",
+    items: [
+      {
+        itemId: "First",
+        amount: "1",
+        comAmt: "0",
+      },
+    ],
+    cartDescription: "",
+    bankCode, // AXIS: 9480
+    accountNo,
+    accountType: "Saving",
+    accountHolderName,
+    ifscCode,
+
+    // TODO: Check if this is required!
+    currency: "INR",
+  };
+}
